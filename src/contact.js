faa907d30f3131de9f606834c2876c73c6690b95
AnnaMizgailo <102236391+AnnaMizgailo@users.noreply.github.com>
2022-12-19 17:06:52 +0300

    Update contact.js

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,19 @@
 export default class Contact {
-    // code here
+    id;
+    phones;
+    lastName;
+    firstName;
+    middleName;
+    comment;
+
+    constructor(id, phones, lastName, firstName, middleName, comment){
+        this.id = id;
+        if(this.phones.isArray()){
+            this.phones = phones;
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.comment = comment;
+    }
 } 
