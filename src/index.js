72406bf1e4c794f5fad4b706f60ae1573cefe071
AnnaMizgailo <102236391+AnnaMizgailo@users.noreply.github.com>
2022-12-21 18:27:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,44 +11,75 @@ import ContactDAO from './contact_dao.js';
     console.log("   list - Get list of contacts");
     console.log("   exit - EXIT");
 
-    process.stdin.setEncoding("utf8");
-    process.stdin.setRawMode(false);
-    process.stdin.on("data", (text) => {
-        if (text === "exit" || text === "exit\r\n") {
-            process.exit(0);
-        }
-        if (text === "add" || text === "add\r\n") {
-            // code here
-            return;
-        }
-        if (text === "delete" || text === "delete\r\n") {
-            // code here
-            return;
-        }
-        if (text === "update" || text === "update\r\n") {
-             // code here
-             return;
-        }
-        if (text === "searchById" || text === "searchById\r\n") {
-            // code here
-            return;
-        }
-        if (text === "searchByPhone" || text === "searchByPhone\r\n") {
-            // code here
-            return;
-        }
-        if (text === "searchByLastName" || text === "searchByLastName\r\n") {
-            // code here
-            return;
-        }
-        if (text === "searchByFirstName" || text === "searchByFirstName\r\n") {
-             // code here
-             return;
-        }
+    let exitFlag = false;
+    while(!exitFlag){
+        process.stdin.setEncoding("utf8");
+        process.stdin.setRawMode(false);
 
-        console.log("Wrong answer. You should write the right statements ;)");
-    });
-    process.stdin.on('end', () => {
-        process.exit(0);
-    });
+        let text = readlineSync.question('');
+            if(text === "exit" || text === "add\r\n"){
+                exitFlag = true;
+                continue;
+            }
+        }
+        process.stdin.on("data", (text) => {
+            if (text === "exit" || text === "exit\r\n") {
+                process.exit(0);
+            }
+            if (text === "add" || text === "add\r\n") {
+                let firstName = readlineSync.question('Insert first name: ');
+                let lastName = readlineSync.question('Insert last name: ');
+                console.log('Type `end` to finish the enter of phone');
+                let endPhonesFlag = false;
+                let phones = [];
+                while(!endPhonesFlag){
+                    let phone = readlineSync.question('Insert phone: ');
+                    if(phone === 'end' || phone === 'end\r\n') break;
+                    if(Contact.validatPhone(phone)){
+                        phones.push(phone);
+                    }else{
+                        console.log("phone is not valid");
+                    }
+                    console.log('phones: ${JSON.stringify(phones)}');
+                    let comment = readlineSync.question('Insert comment: ');
+                }
+                return;
+            }
+            if (text === "delete" || text === "delete\r\n") {
+                // code here
+                return;
+            }
+            if (text === "update" || text === "update\r\n") {
+                    // code here
+                    return;
+            }
+            if (text === "searchById" || text === "searchById\r\n") {
+                // code here
+                return;
+            }
+            if (text === "searchByPhone" || text === "searchByPhone\r\n") {
+                // code here
+                return;
+            }
+            if (text === "searchByLastName" || text === "searchByLastName\r\n") {
+                // code here
+                return;
+            }
+            if (text === "searchByFirstName" || text === "searchByFirstName\r\n") {
+                    // code here
+                    return;
+            }
+
+            console.log("Wrong answer. You should write the right statements ;)");
+        });
+        process.stdin.on('end', () => {
+            process.exit(0);
+        });
+    }
+
+    
 }());
+//JSON.stringify(obj);
+//JSON.parse();
+//readFileSync();
+//writeFileSync();
